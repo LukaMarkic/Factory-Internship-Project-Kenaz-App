ac9c0a88eb892e4810d2fb6425f0be0f6a5b3937
LukaMarkic <73442744+LukaMarkic@users.noreply.github.com>
2023-10-18 02:10:45 +0200

    Adding App.js file.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import { Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/shared/Header';
+import Category from './pages/Category';
+import Footer from './components/footer/Footer';
+import Single from './pages/Single';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/Category' element={<Category />} />
+            <Route exact path='/Single' element={<Single />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
