d77997e2988be211c8e0640b34e44cb3d62eaaf8
LukaMarkic <73442744+LukaMarkic@users.noreply.github.com>
2023-10-25 23:34:00 +0200

    Adding onClose functionality.

--- src/components/ModalWindow.js
+++ src/components/ModalWindow.js
@@ -1,24 +1,25 @@
+import React from "react";
+import "../styles/modalWindow.scss";
 
-import React, { useState } from 'react'
-import '../styles/modalWindow.scss'
+function ModalWindow({ children, title, isOpen, setIsOpen, onClose }) {
+  const closeModal = () => {
+    setIsOpen(false);
+    if (onClose) onClose();
+  };
 
-function ModalWindow({children, title, isOpen, setIsOpen}) {
-   
-    const closeModal = () => {
-        setIsOpen(false);
-    }
-
-    return (
-        <div className={`modal-window ${isOpen ? 'show-modal' : ''}`}>
-            <div className='modal-window-content'>
-                <div className='modal-window-top-banner'>
-                    <h2>{title}</h2>
-                    <button className="close" onClick={closeModal}>&times;</button>
-                </div>
-                {children}
-            </div>
+  return (
+    <div className={`modal-window ${isOpen ? "show-modal" : ""}`}>
+      <div className="modal-window-content">
+        <div className="modal-window-top-banner">
+          <h2>{title}</h2>
+          <button className="close" onClick={closeModal}>
+            &times;
+          </button>
         </div>
-    )
+        {children}
+      </div>
+    </div>
+  );
 }
 
-export default ModalWindow
+export default ModalWindow;
