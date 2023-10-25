e6ae106a3b4c6fc51d3cdce0ba44cceabcc757f8
LukaMarkic <73442744+LukaMarkic@users.noreply.github.com>
2023-10-25 23:31:14 +0200

    Fixing the format.

--- src/components/footer/TwitterFeed.js
+++ src/components/footer/TwitterFeed.js
@@ -1,28 +1,29 @@
-
-import React from 'react'
-import twitterPosts from '../../data/twitterPosts'
-import '../../styles/footer/twitterFeed.scss'
-import twitterBird from '../../images/social-icons/twitter-bird.svg'
+import React from "react";
+import twitterPosts from "../../data/twitterPosts";
+import "../../styles/footer/twitterFeed.scss";
+import twitterBird from "../../images/social-icons/twitter-bird.svg";
 
 function TwitterFeed() {
   return (
-    <div className='twitter-feed-container'>
-        <h2>Twitter Feed</h2>
-        {twitterPosts.map((post, index)=><TwitterPost key={index} post={post}/>)}
+    <div className="twitter-feed-container">
+      <h2>Twitter Feed</h2>
+      {twitterPosts.map((post, index) => (
+        <TwitterPost key={index} post={post} />
+      ))}
     </div>
-  )
+  );
 }
 
-function TwitterPost({post}) {
+function TwitterPost({ post }) {
   return (
-    <div className='twitter-post'>
-        <div>
-            <img src={twitterBird} />
-            <p>{`${post.user} @${post.username}`}</p>
-        </div>
-        <p>{post.post}</p>
+    <div className="twitter-post">
+      <div>
+        <img src={twitterBird} />
+        <p>{`${post.user} @${post.username}`}</p>
+      </div>
+      <p>{post.post}</p>
     </div>
-  )
+  );
 }
 
-export {TwitterFeed, TwitterPost}
+export { TwitterFeed, TwitterPost };
