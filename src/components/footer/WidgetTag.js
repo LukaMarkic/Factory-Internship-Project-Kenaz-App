d5404219ddd9f6a86726732aa9bf40063a6dbf23
LukaMarkic <73442744+LukaMarkic@users.noreply.github.com>
2023-10-25 23:29:36 +0200

    Modifying WidgetTag.js file.

--- src/components/footer/WidgetTag.js
+++ src/components/footer/WidgetTag.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import '../../styles/footer/widgetTag.scss'
+import React, { useState } from "react";
+import "../../styles/footer/widgetTag.scss";
+import styleDisable from "../../styles/modules/disable.module.scss";
 
-function WidgetTag({name, selected, handleClick}) {
-    
-    return (
-        <div className={`widget-tag-container ${selected ? 'selected' : ''}`} onClick={handleClick}>
-            {name}
-        </div>
-    )
+function WidgetTag({ name }) {
+  const [tagSelected, setTagSelected] = useState(false);
+  const handleClick = () => {
+    setTagSelected((prev) => !prev);
+  };
+  return (
+    <div
+      className={`${styleDisable.disableSelect} widget-tag-container ${
+        tagSelected ? "selected" : ""
+      }`}
+      onClick={handleClick}
+    >
+      {name}
+    </div>
+  );
 }
 
-export default WidgetTag
+function WidgetTagContiner({ tags = [] }) {
+  return (
+    <div className="widget-tag-contianer">
+      <h1>Tags Widget</h1>
+      <div>
+        {tags.map((tag, index) => (
+          <WidgetTag key={index} name={tag.name} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export { WidgetTagContiner, WidgetTag };
